50ba3dd6182dcfe62b0928ce00b369ea89470d69
phiggins <pete@peterhiggins.org>
2010-11-03 00:59:48 -0700

    Clean cruft from tests, get them to run on 1.9.2.

--- spec/redis_impersonator_spec.rb
+++ spec/redis_impersonator_spec.rb
@@ -1,4 +1,4 @@
-require 'spec/spec_helper'
+require 'spec_helper'
 
 describe 'RedisImpersonator' do
   let(:impersonator) { RedisImpersonator.new }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,6 @@
 require 'rubygems'
 require 'resque'
-
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'swagger'
-require 'spec'
-require 'spec/autorun'
 
 Spec::Runner.configure do |config|
 
@@ -17,9 +12,7 @@ end
 
 Resque.swagger!
 
-ActiveRecord::Base.establish_connection('adapter' => 'sqlite3', 'database' => 'test.db')
-
-ActiveRecord::Base.connection.drop_table :resque_values rescue puts "unable to drop resque_values, probably because it has not been created yet"
+ActiveRecord::Base.establish_connection('adapter' => 'sqlite3', 'database' => ':memory:')
 ActiveRecord::Base.connection.create_table :resque_values do |table|
   table.column :key,      :string
   table.column :key_type, :string
@@ -28,4 +21,3 @@ end
 
 ActiveRecord::Base.connection.add_index :resque_values, :key
 ActiveRecord::Base.connection.add_index :resque_values, [:key, :key_type]
-
